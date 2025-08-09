580105be7928eae3d287d9a2e406d433b5bfa128
bailarincompadrito <83310118+bailarincompadrito@users.noreply.github.com>
2025-08-09 15:16:34 -0500

    Create App.jsx

---
+++ src/src/src/src/src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ fontFamily: "Arial, sans-serif", padding: "20px" }}>
+      <h1>🕺 Tango Private Lessons</h1>
+      <p>
+        Welcome! This is the first version of your app for booking private tango
+        lessons.
+      </p>
+      <p>
+        Here you will soon be able to choose instructors, book your time slot,
+        and pay online.
+      </p>
+      <hr />
+      <footer style={{ marginTop: "20px", fontSize: "0.9em", color: "#555" }}>
+        Admin Contact: 312 7276293 | jorgenelgiraldo@gmail.com
+      </footer>
+    </div>
+  );
+}
+
+export default App;
